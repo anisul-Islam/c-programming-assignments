d2390e9b5497f8b7cd3f1c49c17373e22c997d86
Anisul Islam <anisul2010s@yahoo.co.uk>
2023-10-15 07:38:29 +0300

    Create switch.c

---
+++ switch.c
@@ -0,0 +1,11 @@
+1.	Write a program that read a digit and display by its spelling.
+  // add the solution here 
+  
+2.	Write a program that read a year and display leap year or not.
+  // add the solution here 
+
+3.	Write a program that read any letter and display vowel or consonant.
+    // add the solution here 
+  
+4.	Write a program that read any number and display its Roman number.
+    // add the solution here 
