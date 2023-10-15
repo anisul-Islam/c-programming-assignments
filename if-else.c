0903e014926edf9677a8e81aa3a4b74187eaaa49
Anisul Islam <anisul2010s@yahoo.co.uk>
2023-10-15 07:34:57 +0300

    Create if-else.c
    added if -else instructions

---
+++ if-else.c
@@ -0,0 +1,47 @@
+// 1.	Write a program that read mark and display pass or fail.
+// Add the solution here
+
+// 2.	Write a program that read mark and display result in grade.
+// Add the solution here
+
+// 3.	Write a program to check a number even or odd.
+// Add the solution here
+
+// 4.	Write a program that read mark and display pass or fail.
+// Add the solution here
+
+// 5.	Write a program that read mark and display result in grade.
+// Add the solution here
+
+// 6.	Write a program to check a number even or odd.
+// Add the solution here
+
+// 7.	Write a program to check a number positive or negative.
+// Add the solution here
+
+// 8.	Write a program that read two numbers and display maximum.
+// Add the solution here
+
+// 9.	Write a program that read two numbers and display minimum.
+// Add the solution here
+
+// 10.Write a program that read three numbers and display maximum.
+// Add the solution here
+
+// 11.Write a program that read three numbers and display minimum.
+// Add the solution here
+
+// 12.Write a program that read three numbers and display medium.
+// Add the solution here
+
+// 13. Write a program that read three numbers (a, b, c) and determine the roots of the quadratic equation.
+// Add the solution here
+
+// 14. Write a program that read any year and display leap or not.
+// Add the solution here
+
+// 15. Write a program that read any letter and display vowel or consonant.
+// Add the solution here
+
+// 16. Write a program that read any letter and display its uppercase or lowercase.
+// Add the solution here
